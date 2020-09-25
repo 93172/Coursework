e412bd1a596bf4dafcd770133846596a1006197b
93172 <93172@farnborough.ac.uk>
2020-09-25 15:41:59 +0100

    Added User/delete/? Doesnt work, method is not allowed

--- src/main/java/controllers/User.java
+++ src/main/java/controllers/User.java
@@ -4,10 +4,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import server.Main;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,16 +13,23 @@ import java.sql.ResultSet;
 @Consumes(MediaType.MULTIPART_FORM_DATA)
 @Produces(MediaType.APPLICATION_JSON)
 
+
+
 public class User {
+
+
+
+
     @GET
     @Path("list")
+
     public String userList() {
         System.out.println("Invoked User.userList()");
         JSONArray response = new JSONArray();
         try {
             PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, Username, Password, Admin, SessionToken FROM Users");
             ResultSet results = ps.executeQuery();
-            while (results.next()==true) {
+            while (results.next() == true) {
                 JSONObject row = new JSONObject();
                 row.put("UserID", results.getInt(1));
                 row.put("Username", results.getString(2));
@@ -40,4 +44,26 @@ public class User {
             return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
         }
     }
+
+    @POST
+    @Path("delete/{UserID}")
+
+    public String deleteUser(@PathParam("UserID") Integer UserID) {
+        System.out.println("Invoked deleteUser()");
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Users WHERE UserID = ?");
+            ps.setInt(1, UserID);
+            ps.execute();
+            return "{\"OK\": \"User deleted\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to delete item, please see server console for more info.\"}";
+        }
+
+    }
+
+
+
+
+
 }
